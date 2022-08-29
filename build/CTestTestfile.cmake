cf8ed9bfee420a997bda922e0d926799b90f0079
Albert <albert3257@live.dk>
2022-08-29 13:30:55 +0200

    initial

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/albert/projects/raspberrypi-class
+# Build directory: /home/albert/projects/raspberrypi-class/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
